1f38f05271552bf7327e589312c0fee7f5638123
Brandy Wills <wills.brandy@gmail.com>
2017-12-01 13:09:31 -0600

    Added CSS and refined templates.

--- land/views.py
+++ land/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Update 
 
 # Create your views here.
 
 def update_list(request):
-    return render(request, 'land/landing_page.html')
+    updates = Update.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'land/landing_page.html', {'updates':updates})
